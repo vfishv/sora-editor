001785e7c58936a0cc8b688a46eb0849c64e5d74
Q <foldervapp@gmail.com>
2023-01-17 10:31:57 +0800

    up

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -29,8 +29,8 @@ public final class Versions {
     public final static int versionCode = 27;
 
     // Platform & Tool versions
-    public final static String buildToolsVersion = "31.0.0";
-    public final static int compileSdkVersion = 31;
+    public final static String buildToolsVersion = "33.0.1";
+    public final static int compileSdkVersion = 33;
     public final static int minSdkVersion = 21;
     public final static int targetSdkVersion = 31;
 
