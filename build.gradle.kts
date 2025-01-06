3ec653bbbaaff0066212cc8817f85bdb9e4d3f62
vfishv <vfishv@gmail.com>
2025-01-06 22:54:55 +0800

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,8 @@ fun Project.configureBaseExtension() {
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
         }
     }
 }
